2388b1eaa0607d50a331110588c7dd3f54324115
Stratton Ashworth <chrissimbarashe1998@gmail.com>
2025-11-06 16:52:44 +0200

    coffee animations done

--- Sources/StrattonAshworth/coffee.swift
+++ Sources/StrattonAshworth/coffee.swift
@@ -28,9 +28,14 @@ struct CoffeeHome: View {
     var body: some View {
         GeometryReader { 
             let size = $0.size
-            //MARK: Since card size is the size of the screen width
+            let cardSize = size.width * 0.8
 
-            let cardSize = size.width
+            LinearGradient(colors: [Color.brown.opacity(0.2), Color.brown.opacity(0.45), Color.brown], startPoint: .top, endPoint: .bottom)
+                .frame(height: 300)
+                .frame(maxHeight: .infinity, alignment: .bottom)
+                .ignoresSafeArea()
+
+                HeaderView()
 
             VStack(spacing: 0) {
                 ForEach(coffees) { coffee in
@@ -43,6 +48,7 @@ struct CoffeeHome: View {
             .offset(y: offsetY)
             .offset(y: -currentIndex * cardSize)
         }
+        .coordinateSpace(name: "SCROLL")
         .contentShape(Rectangle())
         .gesture(
             DragGesture()
@@ -65,6 +71,30 @@ struct CoffeeHome: View {
                 })
         )
         
+    }
+
+    @ViewBuilder func HeaderView() -> some View {
+        //Animated Slider
+
+        GeometryReader {
+            let size = $0.size
+
+            HStack(spacing: 0) {
+                ForEach(coffees) { coffee in
+                    VStack(spacing: 15) {
+                        Text(coffee.title)
+                            .font(.title.bold())
+                            .multilineTextAlignment(.center)
+                        Text(coffee.price)
+                            .font(.title2.bold())
+                    }
+                    .frame(width: size.width)
+                }
+            }
+            .offset(x: currentIndex * -size.width)
+            .animation(.interactiveSpring(response: 0.6, dampingFraction: 0.8, blendDuration: 0.8), value: currentIndex)
+        }
+        .padding(.top, -5)
     }
 }
 
@@ -91,15 +121,29 @@ struct coffeeview: View {
     var size: CGSize
 
     var body: some View {
+        let cardSize = size.width * 0.8 //size of the image
+
+        let maxCardsDisplaySize = size.width * 4
         GeometryReader{ geometry in 
-        let size = geometry.size
+        let _size = geometry.size
+
+        //current card offset
+        let offset = geometry.frame(in: .named("SCROLL")).minY - (size.height - cardSize) 
+        let scale = offset <= 0 ? (offset / maxCardsDisplaySize) : 0
+        let reducedScale = 1 + scale
+        let currentCardScale = offset / cardSize
 
         Image(coffee.imageNaame)
         .resizable()
         .aspectRatio(contentMode: .fit)
-        .frame(width: size.width, height: size.height)
+        .frame(width: _size.width, height: _size.height)
+        .scaleEffect(reducedScale < 0 ? 0.001 : reducedScale, anchor: .init(x: 0.5, y: 1 - (currentCardScale / 2.4))
+        .scaleEffect(offset > 0 ? 1 + currentCardScale : 1, anchor: .top)
+        .offset(y: offset > 0 ? currentCardScale * 200 : 0)
+        //making it more compact
+        .offset(Y: currentCardScale * -130)
         }
-        .frame(height: size.width)
+        .frame(height: cardSize)
     }
 }
 
